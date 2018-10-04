de4b03abdcf29057e5167f95c510b20bf4f9cc86
yuang.zya <yuang.zya@alibaba-inc.com>
2018-10-04 17:43:06 +0800

    feat: 完善项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const html2bigview = require('html2bigview')
-const redis = require('redis')
-const client = redis.createClient()
+// const redis = require('redis')
+// const client = redis.createClient()
 
 async function index (ctx) {
+  const time = new Date().getTime()
+  global.startTime = time
   const url = 'http://g.alicdn.com/ku/bigview.runtime/1.4.9/html2bigview/index.html'
   await html2bigview(ctx, url, {
     zip: true,
-    redisClient: client,
+    // redisClient: client,
     headJs: [],
     tplPath: 'src/tpl'
   })
